7baa58fa22c334cca36372f130d9b6cb0f785daf
Green <Christian_heinzmann@web.de>
2019-10-26 22:38:21 +0200

    timestamp to minutes

---
+++ data/src_time/time_to_minutes.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import csv
+
+
+def time_to_minutes_Parking(timestamp="12:30"):
+
+
+    path_to_data = '../data/Car_Parking_Data.csv'
+
+    with open(path_to_data, mode='w') as csv_file:
+
+
+        print(csv_file)
+        print(csv_file)
