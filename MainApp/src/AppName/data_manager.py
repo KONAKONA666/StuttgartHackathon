40b8f0eb3761c6bd746a17d218ea004e8c303582
HoangND1997 <hoang.d1997@gmail.com>
2019-10-26 21:58:14 +0200

    Added a date picker and data manager for flight data with http get requests

--- MainApp/src/AppName/data_manager.py
+++ MainApp/src/AppName/data_manager.py
@@ -1,12 +1,38 @@
 import requests
+import datetime
 
 class DataManager():
 
-    def get_amsID(self, flight_number , from_time , till_time):
+    # basic flight information as root point for data extraction
+    flight_information = None
+
+    # id of the flight
+    amsID = None
+
+    def get_departure(self):
+
+        URL = "http://fsg-datahub.azure-api.net/legacy/Apps/AirportSTR/Flights/GetDeparture/"+str(self.amsID)
+        HEADERS = {"Ocp-Apim-Subscription-Key":"e9dddf3b02c04ac98d341b24036bc18f"}
+        #PARAMS = {"amsid":self.amsID}
+
+        data = requests.get(url=URL,headers=HEADERS)
+        departure_data = data.json()
+
+        departure = datetime.datetime.strptime(departure_data["Plan"],"%Y-%m-%dT%H:%M:%S")
+
+        return departure
+
+        #datetime.datetime.strptime(time_of_flight, '%d-%m-%Y').date()
+
+
+    def set_flight(self, flight_number, time_of_flight):
+
+        from_time = datetime.datetime.strptime(time_of_flight, '%d/%m/%Y').date()
+        till_time = from_time + datetime.timedelta(days=1)
 
         URL = "http://fsg-datahub.azure-api.net/legacy/Apps/AirportSTR/Flights/Get"
         HEADERS = {"Ocp-Apim-Subscription-Key":"e9dddf3b02c04ac98d341b24036bc18f"}
-        PARAMS = {"from":from_time,"till":till_time,"Name":flight_number}
+        PARAMS = {"from": from_time,"till":till_time,"Name":flight_number}
 
         data = requests.get(url=URL,headers=HEADERS,params=PARAMS)
 
@@ -14,12 +40,16 @@ class DataManager():
 
         for f in flight_list:
                 if f["Name"] == flight_number:
-                        flight_information = f
+                        self.flight_information = f
 
-        print(flight_information)
+        self.amsID = self.flight_information["AmsId"]
 
+    def get_flight_information(self):
+        return self.flight_information
 
 if __name__ == "__main__":
     data_manager = DataManager()
 
-    data_manager.get_amsID("EW 2964","2019-10-26T01:09:21","2019-10-26T21:09:21")
+    data_manager.set_flight("EW 2964","26/10/2019")
+    print(data_manager.get_flight_information())
+    print(data_manager.get_end_time())
--- MainApp/src/AppName/main.py
+++ MainApp/src/AppName/main.py
@@ -2,10 +2,13 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivymd.uix.label import MDLabel
 from kivymd.uix.button import MDFlatButton
+from kivymd.uix.picker import MDDatePicker
 from kivymd.theming import ThemeManager
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivymd.uix.tab import MDTabsBase
+from datetime import datetime
+from data_manager import DataManager
 
 class IntroScreen(Screen):
     pass
@@ -39,24 +42,48 @@ class MainApp(App):
     theme_cls = ThemeManager()
     title = "Main App"
 
+    def show_date_picker(self):
+        MDDatePicker(self.get_date).open()
+
+    def get_date(self, date_obj):
+        self.intro_screen.ids.flight_date_field.text = date_obj.strftime("%d/%m/%Y")
+
+    def set_flight_information(self):
+        flight_number = self.intro_screen.ids.flight_number_field.text
+        flight_date = self.intro_screen.ids.flight_date_field.text
+        print(self.data_manager.set_flight(flight_number,flight_date))
+
+    def set_data(self):
+
+        # get end time = departure time
+        departure_time = self.data_manager.get_departure().strftime("%H:%M")
+
+        # set or calculate the time
+        self.main_tabs.ids.end_time_label.text = departure_time
+
     def build(self):
+
+        self.data_manager = DataManager()
+
         self.theme_cls.them_style = "Light"
         self.theme_cls.primary_palette = "Blue"
         self.theme_cls.accent_palette = "Gray"
 
         # screens
-        main_tabs = GeneralOverViewScreen(name="GeneralOverViewScreen")
-        main_tabs.ids.android_tabs.add_widget(timetableTab())
-        main_tabs.ids.android_tabs.add_widget(journeyTab())
-        main_tabs.ids.android_tabs.add_widget(checkInTab())
-        main_tabs.ids.android_tabs.add_widget(securityTab())
-        main_tabs.ids.android_tabs.add_widget(personalTab())
-        main_tabs.ids.android_tabs.add_widget(boardingTab())
+        self.intro_screen = IntroScreen(name="IntroScreen")
+
+        self.main_tabs = GeneralOverViewScreen(name="GeneralOverViewScreen")
+        self.main_tabs.ids.android_tabs.add_widget(timetableTab())
+        self.main_tabs.ids.android_tabs.add_widget(journeyTab())
+        self.main_tabs.ids.android_tabs.add_widget(checkInTab())
+        self.main_tabs.ids.android_tabs.add_widget(securityTab())
+        self.main_tabs.ids.android_tabs.add_widget(personalTab())
+        self.main_tabs.ids.android_tabs.add_widget(boardingTab())
 
         # ScreenManager to switch different pages
         sm = ScreenManager()
-        sm.add_widget(IntroScreen(name="IntroScreen"))
-        sm.add_widget(main_tabs)
+        sm.add_widget(self.intro_screen)
+        sm.add_widget(self.main_tabs)
 
         return sm
 
